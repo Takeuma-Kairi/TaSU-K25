54c11f9c8352f1c799bcdf32bd51544b7cacba89
Takeuma_Kairi <71685901+Takeuma-Kairi@users.noreply.github.com>
2026-01-02 21:52:54 +0900

    Karai追加

--- source/script.js
+++ source/script.js
@@ -8,6 +8,7 @@ const EVENT_SCRIPT=[
 [function(){window.open("stories/R01/index.html")}],
 [function(){window.open("stories/R02/index.html")}],
 [function(){window.open("stories/Tochino2/index.html")}],
+[function(){window.open("stories/Karai/index.html")}],
 ];
 
 
---
+++ stories/Karai/source/script.js
@@ -0,0 +1,384 @@
+const SECTION_ARR = ["page", "item", "setting"];
+const BUTTON_ARR = ["button_page", "button_item", "button_setting"];
+
+//ライトかダークか、カラーモードを保存しておく
+let isDarkMode = false; //デフォルトのテーマがダークテーマか否か。初期値はfalse
+let color_theme = "light"; //現在のテーマはdarkかlightか。初期値はlight
+
+let page_num = 0;
+
+const STORY_LENGTH=40;
+
+let item_haveArr=[];
+let flagArr=[];
+const TAGARR={"kanban1": 5, 
+"kanban2": 6, 
+"into": 7, 
+"bunki": 9, 
+"kansatu": 10, 
+"yoko": 11, 
+"yasiki": 15, 
+"outmituka": 19, 
+"mae": 24, 
+"mae2": 27, 
+"hidari": 30, 
+"mae3": 32, 
+"hidari2": 33, 
+};
+
+const EVENT_SCRIPT=[
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){tob("into")}, function(){tob("kanban2")}, ],
+[function(){tob("into")}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){tob("mae")}, function(){tob("yoko")}, function(){tob("kansatu")}, ],
+[function(){tob("bunki")}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){tob("outmituka")}, function(){tob("yasiki")}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){restart()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){restart()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){tob("mae2")}, function(){tob("hidari")}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){restart()}, ],
+[function(){nxp()}, ],
+[function(){tob("mae3")}, function(){tob("hidari2")}, ],
+[function(){tob("mae2")}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){nxp()}, ],
+[function(){restart()}, ],
+];
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  isDarkMode = darkModeMediaQuery.matches;
+
+  // 初期状態でどちらのテーマなのかを取得、それに合わせてページのテーマを決める
+  color_theme = isDarkMode ? "dark": "light";
+  
+  document.documentElement.setAttribute("theme", color_theme);
+  change_color(color_theme);
+  
+  //スクロール高速化のために必要らしい内容。
+  //touchstart？もしくはontouchstartは、スマホでホバーを実現するために入れている。
+  document.getElementById("body").addEventListener('touchstart', function(){}, {passive: true});
+
+  make_selection_event();
+});
+
+
+
+//カラーテーマの変更
+function change_color(color){
+  color_theme = color;
+  
+  // ライトかダークか明示されないなら、デフォルトの設定に合わせる
+  if(color_theme != "light" && color_theme != "dark"){
+    color_theme = isDarkMode ? "dark" : "light";
+  }
+  
+  document.documentElement.setAttribute("theme", color_theme);
+  
+  // 画像の変更
+  document.getElementById("icon_setting").src = "../../source/icon1_" + color_theme + ".png"
+  document.getElementById("icon_page").src = "../../source/icon2_" + color_theme + ".png"
+  document.getElementById("icon_item").src = "../../source/icon4_" + color_theme + ".png"
+  
+  let selection_iconArr = document.getElementsByClassName("selection_icon");
+  for(i=0; i<selection_iconArr.length; i++){
+    selection_iconArr[i].src = "../../source/icon3_" + color_theme + ".png";
+  }
+}
+
+
+//設定タブの「最初から始める」ボタン
+document.getElementById("restart_button").addEventListener("click", () => {
+  var res = confirm("最初から始めますか？進行は保存されません");
+  if(res){
+    restart();
+  }
+});
+
+
+
+//ページの選択肢ボタンに、clickイベントを配置する。
+function make_selection_event(){
+  for(let i=0; i<EVENT_SCRIPT.length; i++){
+  
+  /*よくわからないが、単純にfor文でaddEventListenerを制御しようとすると、ループが終わり切ったカウンタ変数の値しか反映されず、ちっとも動かない様子。
+  高階関数でスコープの問題をなんとかするしかない。*/
+  
+  (function(n){
+    //var selectionArr = EVENT_SCRIPT[i];
+  
+    for(let j=0; j<EVENT_SCRIPT[i].length; j++){
+      
+      (function(m){document.getElementById("btn_" + i + "_" + j).addEventListener("click",
+        function (e) {
+          EVENT_SCRIPT[i][j]();
+        });
+      })(j);
+      
+    }
+    
+  })(i);
+}
+}
+
+
+// セーブデータをロードするボタン
+document.getElementById("save_submit").addEventListener("click", () => {
+  var keyword1 = document.getElementById("save_keyword1").value;  //1. ページ番号データ
+  document.getElementById("save_keyword1").defaultValue=keyword1;
+  
+  var keyword2 = document.getElementById("save_keyword2").value;  //2. アイテムデータ
+  document.getElementById("save_keyword2").defaultValue=keyword2;
+  
+  var keyword3 = document.getElementById("save_keyword3").value;  //3. フラグデータ
+  document.getElementById("save_keyword3").defaultValue=keyword3;
+  
+  keyword1Int=parseInt(keyword1);
+  keyword2Int=parseInt(keyword2);
+  keyword3Int=parseInt(keyword3);
+  
+  //空文字だと不適
+  if(isNaN(keyword1Int) || isNaN(keyword2Int)  || isNaN(keyword3Int)){
+    alert("セーブデータが不適です。")
+  }else{
+    alert("セーブデータを反映しました。");
+    mov(keyword1Int);
+    load_itemdata(keyword2Int.toString(2));
+    load_flagdata(keyword3Int.toString(2));
+    
+    change_article("page");
+  }
+});
+
+
+
+/*アイテムデータについて
+アイテムのセーブデータは10進数で管理していますが、これは2進数がベースです。
+たとえば、6種類のアイテム（０，１，２，３，４，５番まである）のうち、１，３番を持っている場合、
+001010
+のように表記されることを期待します。1がもっていて、0はもっていないという意味です。右からの方向です。
+実際は、左の桁の0はカットされて、1010となってしまい、4番、5番のアイテム所持データが消えます。
+これを補正するため、load_itemdataでは、桁をひっくり返しておき、ついでに配列にしておきます。
+
+001010 → 010100 → ['0', '1', '0', '1', '0', '0']
+
+左からfor文で走査しながら、アイテムをもっている／もっているの評価をします。
+なお、5番目より後のアイテムを考える時、0も1もかいていない状況になり、undefinedなので、それは、もっていない判定にします。
+
+ちなみにこのデータを10進数で表すと「26」です。
+
+なぜ10進数で表すかと言うと、仮に25種類のアイテムを
+11001011000111111101
+このように持っていた場合、このままの2進数データを覚えるより、
+10進数で「831997」としたほうが、覚えやすさがマシだと思ったからです。
+*/
+//2進数のアイテムセーブデータを読んで、もっている／もっていないを更新する
+function load_itemdata(binaryStr){
+  dataArr = binaryStr.split("").reverse();
+  for(var i=0; i<item_haveArr.length; i++){
+
+    if(dataArr[i] == "1"){
+      item_haveArr[i] = true;
+    }else{ 
+      //"0"があった場合 
+      //あと、dataArrの要素オーバーフローでundefinedが返された場合も、もっていない判定
+      item_haveArr[i] = false;
+    }
+  }
+  
+  item_update();
+}
+
+//フラグバージョンのセーブデータ読み込み
+function load_flagdata(binaryStr){
+  dataArr = binaryStr.split("").reverse();
+  for(var i=0; i<flagArr.length; i++){
+
+    if(dataArr[i] == "1"){
+      flagArr[i] = true;
+    }else{ 
+      //"0"があった場合 
+      //あと、dataArrの要素オーバーフローでundefinedが返された場合も、false判定
+      flagArr[i] = false;
+    }
+  }
+}
+
+// 現在のセーブデータを出力する
+function savedata_write(){
+  // アイテムの二進数化
+  var item_binaryStr = "";
+  for(var i=0; i<item_haveArr.length; i++){
+    if(item_haveArr[i]){
+      item_binaryStr = "1" + item_binaryStr;
+    }else{
+      item_binaryStr = "0" + item_binaryStr;
+    }
+  }
+  
+  //フラグの二進数化
+  var flag_binaryStr = "";
+  for(var i=0; i<flagArr.length; i++){
+    if(flagArr[i]){
+      flag_binaryStr = "1" + flag_binaryStr;
+    }else{
+      flag_binaryStr = "0" + flag_binaryStr;
+    }
+  }
+  
+  //アイテム、フラグが１つもないと、""となってセーブデータを書き込めなくなるため、かわりに"0"を入れておく
+  if(item_binaryStr ==""){
+    item_binaryStr ="0";
+  }
+  
+  if(flag_binaryStr ==""){
+    flag_binaryStr ="0";
+  }
+  document.getElementById("now_save_keyword1").innerText = page_num;
+  document.getElementById("now_save_keyword2").innerText = parseInt(item_binaryStr, 2);
+  document.getElementById("now_save_keyword3").innerText = parseInt(flag_binaryStr, 2);
+}
+
+
+
+// ページと設定のアーティクル切り替え
+function change_article(name){
+  // いったん全部選択されていない状態にリセットしてから
+  SECTION_ARR.map((value)=>{document.getElementById(value).style.display="none"});
+  BUTTON_ARR.map((value)=>{document.getElementById(value).className="tab_button"});
+  
+  // 個々を選択状態にする
+  document.getElementById(name).style.display="inline-block";
+  document.getElementById("button_" + name).className="tab_button selected";
+  
+  //設定ページに飛んだら、セーブデータも表示しておく
+  if(name=="setting"){
+    savedata_write();
+  }
+}
+
+
+
+// ページ移動
+function mov(i){
+  //無効なのは、0未満の数字
+  if(i < STORY_LENGTH && i>=0){
+  document.getElementById("page_" + page_num).className="hidden_page";
+  document.getElementById("page_" + i).className="hidden_page open_page";
+  page_num = i;
+  
+  }else{
+    alert("エラー：存在しないページに飛ぼうとしています！");
+  }
+}
+
+//次のページへ
+function nxp(){
+  mov(page_num+1);
+}
+
+//前のページへ
+function bcp(){
+  mov(page_num-1);
+}
+
+//タグ番号で飛ぶ
+function tob(i){
+  mov(TAGARR[i]);
+}
+
+//アイテムの表示
+function item_update(){
+  item= document.getElementById("item_div");
+  
+  
+  //アイテムを１つももっていないか否かの判定をする
+  ifhave_any_item = false;
+  
+  for(var i=0; i < item_haveArr.length; i++){
+    if(item_haveArr[i]){
+      ifhave_any_item = true;
+      document.getElementById("item_" + i).className="item_p open_item";
+    }else{
+      document.getElementById("item_" + i).className="item_p hidden_item";
+    }
+  }
+  
+  // 走査しても、何もアイテムを持っていなければ
+  if(!ifhave_any_item){
+    document.getElementById("noitem").className="item_p open_item";
+  }else{
+    document.getElementById("noitem").className="item_p hidden_item";
+  }
+}
+
+
+//アイテムを持っているか否か
+function ifhave(i){
+  return(item_haveArr[i]);
+}
+
+//アイテムを得る
+function getitem(i){
+  item_haveArr[i]=true;
+  item_update();
+}
+
+//アイテムを失う
+function loseitem(i){
+  item_haveArr[i]=false;
+  item_update();
+}
+
+//一番初めから再開
+function restart(){
+  //アイテムを失う
+  for(var i=0; i< item_haveArr.length; i++){
+    item_haveArr[i] = false;
+  }
+  item_update();
+
+  mov(0);
+}
+
+//フラグが立っているか
+function iflag(i){
+  return(flagArr[i]);
+}
+
+//フラグを立てる
+function onflag(i){
+  flagArr[i]=true;
+}
+
+
+//フラグをおろす
+function offlag(i){
+  flagArr[i]=false;
+}
+
